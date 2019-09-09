a0b9811f6ed5790549ef63e78581031a9c6c0201
George Yord <george.yord@gmail.com>
2019-09-09 12:26:44 +0300

    Take input print Hello world

---
+++ src/try1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func handleError(message string) {
+	panic(errors.New(fmt.Sprintf("Error: %s", message)))
+}
+
+func getName() (name string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		return strings.TrimSpace(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	return
+}
+
+func main() {
+	fmt.Println("Please enter your name.")
+	name := getName()
+	if name == "" {
+		handleError("Name is required")
+	}
+	fmt.Printf("Hello %s!\n", strings.Title(name))
+}
